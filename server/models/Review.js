fa93230e72ba523509f290b667852c899cd40f29
mustafasigad <janodones@gmail.com>
2026-06-02 19:18:31 +0100

    Add guestName to Review model

--- server/models/Review.js
+++ server/models/Review.js
@@ -1,1 +1,10 @@
-const mongoose=require('mongoose');module.exports=mongoose.model('Review',new mongoose.Schema({business:{type:mongoose.Schema.Types.ObjectId,ref:'Business',required:true},user:{type:mongoose.Schema.Types.ObjectId,ref:'User'},rating:{type:Number,required:true,min:1,max:5},text:{type:String,required:true},language:{type:String,default:'so'},verified:{type:Boolean,default:false}},{timestamps:true}));
+﻿const mongoose=require('mongoose');
+module.exports=mongoose.model('Review',new mongoose.Schema({
+  business:{type:mongoose.Schema.Types.ObjectId,ref:'Business',required:true},
+  user:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+  guestName:{type:String,default:'Guest'},
+  rating:{type:Number,required:true,min:1,max:5},
+  text:{type:String,required:true},
+  language:{type:String,default:'so'},
+  verified:{type:Boolean,default:false}
+},{timestamps:true}));
